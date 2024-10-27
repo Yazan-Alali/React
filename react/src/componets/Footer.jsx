79a9a0d1d6a5657030b6bfaafaa82357b5ccbd8a
Yazan Alali <yzn.2@hotmail.com>
2024-10-27 20:11:36 +0100

    myfirstcomponets

---
+++ react/src/componets/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+
+export default function Footer() {
+  return (
+    <div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <SearchForm />
+        
+    </div>
+ 
+  )
+}
---
+++ react/src/componets/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function hero() {
+  return (
+    <section>
+    <div className="container py-5">
+      <h1>Köp grejer av oss!</h1>
+      <button className="btn btn-primary">Boka tid nu!</button>
+    </div>
+  </section>
+  )
+}
+
